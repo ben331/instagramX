6a4725d984daf3c82d3e9dbf0ea704227d27e44c
Benjamin S <benjamin.silva1@u.icesi.edu.co>
2022-03-15 12:51:34 -0500

    Splash Screen

--- app/src/main/java/com/example/instagramx/MainActivity.kt
+++ app/src/main/java/com/example/instagramx/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_InstagramX)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
